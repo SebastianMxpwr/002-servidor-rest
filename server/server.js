210c03d8306723966241d3b2fcc9c69b97584a10
Sebastian <190807@utags.edu.mx>
2020-11-22 23:54:51 -0600

    Conexion vista a mongodb

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose =  require('mongoose');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,7 +10,7 @@ app.use(bodyParser.json())
 
 
 app.get('/', function(req, res){
-    res.send('<h1>HOLIS</h1>');
+    res.send('<h1>Bienvenido a mi servidor rest (localhost)</h1>');
 })
 
 app.get('/usuario', function(req, res){
@@ -61,6 +62,17 @@ app.delete('/usuario/:id', function(req, res){
     })
 
 })
+
+mongoose.connect('mongodb://localhost:27017/cafeteria',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err,res) => {
+  
+  if(err) throw err;
+  console.log('Base de datos Online');
+});
 
 app.listen(process.env.PORT, () => {
     console.log('El servidor esta en linea por el puerto ', process.env.PORT)
